5a6aa06be47455fe0edea89d910c94264f4e84a4
casethebase <trahan.casey.c@gmail.com>
2018-07-19 20:00:09 -0500

    fixing stuff

--- models/hangs.js
+++ models/hangs.js
@@ -1,28 +1,13 @@
+'use strict';
 module.exports = function(sequelize, DataTypes) {
-  var Post = sequelize.define("Post", {
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
-    body: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      len: [1]
-    }
-  });
+  var post = sequelize.define('post', {
+    title: DataTypes.STRING,
+    body: DataTypes.STRING
+  }, {});
 
-  Post.associate = function(models) {
-    // We're saying that a Post should belong to an Author
-    // A Post can't be created without an Author due to the foreign key constraint
-    Post.belongsTo(models.Author, {
-      foreignKey: {
-        allowNull: false
-      }
-    });
-  };
+post.associate = function(models) {
+  post.belongsTo(models.user,{as : 'user', foreignKey: 'userId'})
+}
 
-  return Post;
+  return post;
 };
--- server.js
+++ server.js
@@ -37,4 +37,4 @@ db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
-// });
+});
