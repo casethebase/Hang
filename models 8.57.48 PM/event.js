1dbdaed92d5a556b45f48f32ba8ce92055c911b7
Em Karimifar <em.karimifar@gmail.com>
2018-07-19 21:32:05 -0500

    deleting all

--- models 8.57.48 PM/event.js
+++
@@ -1,9 +0,0 @@
-// var Event = sequelize.define("event", {
-//     eventName: Sequelize.STRING,
-//     eventDate: Sequelize.STRING,
-//     participant: Sequelize.STRING
-// });
-
-// Event.sync();
-
-// module.exports = Event;
