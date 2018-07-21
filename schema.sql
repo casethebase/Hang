d90313639a49ee8e8fb57be34f85dadcf0ba8997
Casey Trahan <26654827+casethebase@users.noreply.github.com>
2018-07-21 14:10:12 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -4,25 +4,25 @@ USE hangdb;
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users
-(
-    id int NOT NULL,
+   (
+	id int NOT NULL,
 	userID int NOT NULL AUTO_INCREMENT,
 	username varchar(255) NOT NULL,
-    email varchar(255) NOT NULL,
-    password varchar(255) NOT NULL,
-    createdAt varchar(20) NOT NULL,
-    updatedAt varchar(20) NOT NULL,
+    	email varchar(255) NOT NULL,
+    	password varchar(255) NOT NULL,
+    	createdAt varchar(20) NOT NULL,
+    	updatedAt varchar(20) NOT NULL,
 	PRIMARY KEY (userID)
-);
+   );
 
 DROP TABLE IF EXISTS posts;
 CREATE TABLE posts
-(
+   (
 	id int NOT NULL AUTO_INCREMENT,
-    userID int NOT NULL,
-    title varchar(100) NOT NULL,
-    body varchar(1000) NOT NULL,
+    	userID int NOT NULL,
+    	title varchar(100) NOT NULL,
+    	body varchar(1000) NOT NULL,
 	createdAt varchar(20) NOT NULL,
-    updatedAt varchar(20) NOT NULL,
-    PRIMARY KEY (id)
-);
+    	updatedAt varchar(20) NOT NULL,
+    	PRIMARY KEY (id)
+   );
