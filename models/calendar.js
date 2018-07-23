a6935c211ad2f9f3ecd72475468a9559a0858f5a
Hecky <thechrisheckendorn@gmail.com>
2018-07-22 21:32:02 -0500

    adding calendar.js model and calendar form

---
+++ models/calendar.js
@@ -0,0 +1,14 @@
+module.exports = function(sequelize, DataTypes) {
+    var Calendar = sequelize.define("User", {
+        eventTitle: DataTypes.STRING,
+        dateStart: DataTypes.STRING,
+        dateEnd: DataTypes.STRING,
+        timeStart: DataTypes.STRING,
+        timeEnd: DataTypes.STRING,
+        userID: DataTypes.STRING
+    });
+    Calendar.sync({force: false})
+    return Calendar;
+
+    };
+
